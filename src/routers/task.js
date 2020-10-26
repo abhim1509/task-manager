4f1a7c2eaf89174a18ab98b0c956aed5d3f3f74b
Abhinav Makker <43211436+abhim1509@users.noreply.github.com>
2020-10-27 01:49:03 +0530

    Add files via upload

---
+++ src/routers/task.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+const User = require('../models/user')
+
+router.post('/tasks', async(req, res)=>{
+    try{
+        const task = new Task(req.body)
+        await task.save()
+        res.status(201).send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+        
+})
+
+router.get('/tasks', async(req, res)=>{
+    try{
+        await Task.find()
+        res.status(200).send(users)        
+    }catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/tasks/:id', async (req, res)=>{
+    try{
+        const _id= req.params
+        await Task.findById(_id)
+        if(!user) 
+            return res.status(404).send(e)
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates=['description', 'completed']
+    const isValidOperation = updates.every((update)=>{
+        return allowedUpdates.includes(update)
+    })//called for every item.
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true}) 
+        //3rd param: options, the original user with updates applied, runs validator to check the validations.
+        if(!task){
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    }   catch(e){ //reach to catch when exceptional case runs.
+        res.status(404).send(e)
+    }
+})
+
+router.delete('tasks/:id', async(req, res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) return res.status(404).send()
+        res.send(user)
+    }   catch(e){ //reach to catch when exceptional case runs.
+        res.status(404).send(e)
+    }
+})
+
+module.exports = router
---
+++ src/routers/user.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const { update } = require('../models/user')
+const router = new express.Router()
+const User = require('../models/user')
+
+router.post('/users', async(req, res) => {
+    try{
+        const user = new User(req.body)
+        await user.save()
+        res.status(201).send(user)
+        
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', async (req, res)=>{
+    try{
+        await User.find()
+        res.status(200).send(users)    
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.get('/users/:id', async(req, res)=>{
+    try{
+        const _id= req.params
+        await User.findById(_id)
+        if(!user) 
+            return res.status(404).send(e)
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/users/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates=['name', 'email','password','age']
+    const isValidOperation = updates.every((update)=>{
+        return allowedUpdates.includes(update)
+    })//called for every item.
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true}) 
+        //3rd param: options, the original user with updates applied, runs validator to check the validations.
+        if(!user){
+            return res.status(404).send()
+        }
+        res.status(200).send()
+    }   catch(e){ //reach to catch when exceptional case runs.
+        res.status(404).send(e)
+    }
+})
+
+router.delete('users/:id', async(req, res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) return res.status(404).send()
+        res.send(user)
+    }   catch(e){ //reach to catch when exceptional case runs.
+        res.status(404).send(e)
+    }
+})
+
+
+module.exports = router
