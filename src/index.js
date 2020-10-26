e0eed154b6541ccc89369ed1f4cce582d70103da
Abhinav Makker <43211436+abhim1509@users.noreply.github.com>
2020-10-27 01:43:02 +0530

    Updated index file

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 const express = require('express')
 require('./db/mongoose')
 
-const User = require('./models/User')
-const Task = require('./models/Task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
 
 const app = new express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get('/users', (req, res)=>{
-    User.find().then((users)=>{
-        res.status(200).send(users)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })    
-})
-
-
-app.post('/tasks', (req, res)=>{
-    const task = new Task(req.body)
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
+app.use(userRouter) //Registering router
+app.use(taskRouter) //Registering router
 
 app.listen(port, ()=>{
     console.log('UP!!'+port)
